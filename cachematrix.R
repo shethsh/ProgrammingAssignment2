63260599c065a9df7540124fc5054c9c146c04b1
shethsh <shethsh@gmail.com>
2014-04-26 14:43:42 +0530

    Adding more comments

--- cachematrix.R
+++ cachematrix.R
@@ -9,6 +9,7 @@
 
 ## makeCacheMatrix is an R function which is able to cache inverse of the given matrix 
 ## x represents matrix and inverse represents the inverse of the matrix
+## set Inverse is the setter method for inverse and getInverse is the getter method for the Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
@@ -29,7 +30,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## However, it first checks to see if the inverse has already been calculated
 ## If so, it gets the inverse from the cache and skips the computation
 ## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setInverse function.
-## We took advantage of the scoping rules of the R language and how they can be manipulated to preserve state inside of an R object to write these functions
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
